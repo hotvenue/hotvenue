60bc5b7efa27445103df46cbe4ce76195aafd1c9
Zweer <n.olivieriachille@gmail.com>
2019-03-27 10:04:33 +0100

    Added the db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
+import { config } from 'node-config-ts';
+
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  // @ts-ignore
+  imports: [TypeOrmModule.forRoot({
+    ...config.database,
+
+    entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
